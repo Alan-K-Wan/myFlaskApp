5dd5758d2ec45b120a1fdb644adf23c7de855473
Alan-K-Wan <97392979+Alan-K-Wan@users.noreply.github.com>
2022-04-30 15:34:25 +0800

    setup environment variables

--- app/routes.py
+++ app/routes.py
@@ -2,5 +2,5 @@ from app import app #from app the folder, import app the package
 
 @app.route('/')
 @app.route('/index')
-function index():
-    return "Hello world!" #return a String
+def index():
+    return "Hello world?????" #return a String
