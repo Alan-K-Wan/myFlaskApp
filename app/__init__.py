69398fcb48621b2ba0a096d98657bd9da3da1d0e
Alan <alan@DerekPC.localdomain>
2022-05-09 19:17:17 +0800

    yes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,18 @@
+import os
+
 from flask import Flask
+from flask import Config
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app=Flask(__name__) #create an instance of Flask class
+app.config.from_object(Config)
 
-from app import routes #from the app folder import the routes.py file
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models #from the app folder import the routes.py file
